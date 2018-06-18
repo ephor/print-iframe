989f3546b8c9abf1581a4cdca44b62aa4637467f
ephor <ephordnb@gmail.com>
2018-06-18 15:01:39 +0300

    init commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const printIframe = content => {
+  const ifrm = document.createElement("iframe");
+  ifrm.setAttribute("id", "printFrame");
+  ifrm.setAttribute("name", "printFrame");
+  ifrm.style.width = "0px";
+  ifrm.style.height = "0px";
+  document.body.appendChild(ifrm);
+  const printFrame = window.frames["printFrame"];
+  printFrame.document.write(content);
+  printFrame.document.write(
+    "<script>window.onload = () => { window.print(); }</script>"
+  );
+  printFrame.document.close();
+  setTimeout(() => {
+    // need to remove print elements on the next tick otherwise print preview doesn't always display styles
+    ifrm.remove();
+  }, 0);
+};
+
+export default printIframe;
